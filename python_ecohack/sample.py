220f24c3b08b24ddaedbe1197c50b1a277452a18
dschurholz <daniel.schurholz@gmail.com>
2014-05-09 23:14:18 -0500

    Adding python scripts for the ecohack.

---
+++ python_ecohack/sample.py
@@ -0,0 +1,14 @@
+from amigocloud_helper import login, fetch_url
+
+if __name__ == '__main__':
+    login_url = 'http://www.amigocloud.com/api/v1/login/'
+    email = 'account.email@amigocloud.com'
+    password = 'accountpassword'
+
+    # Logging in
+    context = login(login_url, email, password)
+    print context['my_personal_info']
+
+    result = fetch_url(context,
+                       "http://www.amigocloud.com/api/v1/users/3/projects/104")
+    print result
