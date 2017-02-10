733ff0f3bd9f36cf0a50e4319b613da2726960f8
Ragi Yaser Burhum <ragi@burhum.com>
2017-02-09 17:21:55 -0800

    Add example of spatial query

---
+++ python/query_spatial.py
@@ -0,0 +1,33 @@
+from pprint import pprint
+from amigocloud import AmigoCloud
+
+# Use amigocloud version 1.0.5 or higher to login with tokens
+# This will raise an AmigoCloudError if the API token is invalid or has expired
+ac = AmigoCloud(token='<your token>')
+
+# For examples of how to get these values, see simple_example2.py
+PROJECT_OWNER = 1
+PROJECT_ID = 14098
+DATASET_ID = 84746
+
+#API endpoint
+sql_url = '/users/{user_id}/projects/{project_id}/sql'.format(
+    user_id=PROJECT_OWNER, project_id=PROJECT_ID
+)
+
+# find all rows that intersect the hard coded point
+query = """
+SELECT * 
+FROM dataset_{dataset_id} 
+WHERE ST_Intersects(wkb_geometry, ST_PointFromText('POINT(-117.150727812638 32.7068451387017)', 4326))
+""".format(dataset_id = DATASET_ID)
+
+response = ac.get(sql_url, {'query': query, 
+                            'dataset_id': DATASET_ID})
+
+# print schema of response
+pprint(response['columns'])
+
+# print row contents
+pprint(response['data'])
+
