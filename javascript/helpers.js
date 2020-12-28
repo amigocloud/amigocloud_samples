2dc139cc2bb5642ebb4db4f84043f92e9799d38c
Arturo Cuya <arturocuya099@gmail.com>
2020-12-28 15:06:16 -0500

    Finished openspatial demo

--- javascript/helpers.js
+++ javascript/helpers.js
@@ -20,3 +20,13 @@ export const waitForJob = async (jobId, jobsBaseUrl, API_TOKEN) => {
 
   return result
 }
+
+export const getFirstMap = async (BASE_URL, PROJECT_UUID, API_TOKEN) => {
+  const MAPS_URL = `${BASE_URL}/api/v1/projects/${PROJECT_UUID}/maps?token=${API_TOKEN}`
+  const mapsRaw = await axios.get(MAPS_URL)
+  const map = mapsRaw.data.results[0]
+
+  if (!Boolean(map)) throw 'No map found. Create one in the original dataset'
+
+  return map
+}
--- javascript/openspatial-demo.js
+++ javascript/openspatial-demo.js
@@ -2,20 +2,19 @@ import fs from 'fs'
 
 import axios from 'axios'
 import FormData from 'form-data'
-import {waitForJob} from './helpers.js'
+import { waitForJob, getFirstMap } from './helpers.js'
 
-// Required constant values
+// Required constant values from the user
 const API_TOKEN = 'A:CzjSMcgMZil54agXnwo2msoNqhQklNGZh6bUyt'
 const ORGANIZATION_UUID = '9820f3db-7745-4c46-98ba-8824363e07db'
-const PROJECT_UUID = 'acca4d97-7aef-4b0a-a6f5-e060c8e0aabc'
+const PROJECT_UUID = 'bd69dcb4-2d78-4c7b-9f7c-a5898cc95da7'
 const USER_UUID = '6df02d58-8765-411f-bc82-5ca7e369ed7c'
 const FILE_PATH = './Parks.zip'
 
 const BASE_URL = 'http://localhost'
 const TOOLS_URL = `${BASE_URL}/api/v2/tools/run?token=${API_TOKEN}`
 const JOBS_BASE_URL = `${BASE_URL}/api/v1/me/jobs`
-const UPLOAD_URL = `${BASE_URL}/api/v1/projects/${PROJECT_UUID}/datasets/upload?token=${API_TOKEN}`
-const MAPS_URL = `${BASE_URL}/api/v1/projects/${PROJECT_UUID}/maps?token=${API_TOKEN}`
+
 
 const TOOL_INPUT = {
   clone_maps: true,
@@ -23,18 +22,16 @@ const TOOL_INPUT = {
   user_uuid: USER_UUID
 }
 
-const checkAOI = async () => {
-  const mapsRaw = await axios.get(MAPS_URL)
-  const initialMap = mapsRaw.data.results[0]
+const openspatial_demo = async () => {
+  // 1. Check initial Area of Interest (Bounding box)
 
-  if (!Boolean(initialMap)) throw 'No map found. Create one in the original dataset'
+  console.log('For initial map')
+  const initialMap = await getFirstMap(BASE_URL, PROJECT_UUID, API_TOKEN)
 
-  console.log('Map AOI:', initialMap.boundingbox)
-  console.log('Map view:', initialMap.view)
-}
+  console.log('Area of Interest:', initialMap.boundingbox)
+  console.log('View:', initialMap.view)
 
-const demo = async () => {
-  // 1. Clone Project "OpenSpatial Demo"
+  // 2. Clone Project "OpenSpatial Demo"
 
   const cloneResult = await axios.post(
     // Endpoint
@@ -49,46 +46,51 @@ const demo = async () => {
 
   const cloneJobId = cloneResult.data.job_id
 
+  console.log('Cloning...')
   const jobResult = await waitForJob(cloneJobId, JOBS_BASE_URL, API_TOKEN)
 
   if (!Boolean(jobResult) || jobResult.status === 'FAILURE') throw 'Error while polling clone job'
-  
-  // 2. Check initial Area of Interest (Bounding box)
 
-  console.log('For initial map')
-  await checkAOI()
+  const clonedProjectUUID = jobResult.extra.output.cloned_project_uuid
+  const uploadToClonedProjectUrl = `${BASE_URL}/api/v1/projects/${clonedProjectUUID}/datasets/upload?token=${API_TOKEN}`
 
   // 3. Upload new data, overwriting the old data
 
   const uploadFormData = new FormData()
-  const newDatasetFile = await fs.promises.readFile(FILE_PATH, 'utf8')
+  const formHeaders = uploadFormData.getHeaders()
+  const newDatasetFile = fs.createReadStream(FILE_PATH)
 
   uploadFormData.append('datafile', newDatasetFile, 'Parks.zip')
   uploadFormData.append('dataset_write_action', 'overwrite')
   uploadFormData.append('csv_props.delimiter', ',')
   uploadFormData.append('csv_props.decimal_separator', '.')
-  uploadFormData.append('process_now', 'true')
-
-  console.log('uploadFormData', uploadFormData)
 
   let uploadResult
   try {
-    uploadResult = await axios.post(UPLOAD_URL, uploadFormData)
+    uploadResult = await axios.post(uploadToClonedProjectUrl, uploadFormData, { headers: formHeaders })
   } catch (error) {
-    console.log(error.response ? error.response.data : error.response)
+    console.log(error.response ? error.response.data : error)
   }
 
   if (!Boolean(uploadResult)) throw 'Error while uploading new dataset file'
-  
 
-  const uploadResultJobId = uploadResult.data.job_id
+  console.log('Uploading overwrite file...')
+  const uploadResultJobId = uploadResult.data.job
   const uploadJobResult = await waitForJob(uploadResultJobId, JOBS_BASE_URL, API_TOKEN)
 
   if (!Boolean(uploadJobResult) || jobResult.status === 'FAILURE') throw 'Error while polling upload job'
 
-  // 4. Check new Area of Interest
+  
   console.log('For overwritten map')
-  await checkAOI()
+  let clonedMap = await getFirstMap(BASE_URL, clonedProjectUUID, API_TOKEN)
+
+  // Clear the map's cache so the AOI is correctly updated
+  await axios.post(`${clonedMap.purge_cache}?token=${API_TOKEN}`, {})
+
+  // 4. Check new Area of Interest
+  clonedMap = await getFirstMap(BASE_URL, clonedProjectUUID, API_TOKEN)
+  console.log('Area of Interest:', clonedMap.boundingbox)
+  console.log('View:', clonedMap.view)
 }
 
-demo()
+openspatial_demo()
